c531fe45c574e914f575c4b5fed9204fa679bda1
Grynn <vishal.doshi@gmail.com>
2011-12-03 03:40:31 +0530

    Initial Commit

---
+++ Program.cs
@@ -0,0 +1,313 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Data.OleDb;
+using System.Globalization;
+using System.IO;
+using System.Text.RegularExpressions;
+
+namespace locate
+{
+    class Program
+    {
+        // Connection string for Windows Search
+        const string connectionString = "Provider=Search.CollatorDSO;Extended Properties=\"Application=Windows\"";
+        static bool OptModified = false;
+        static bool OptSize = false;
+        static bool OptUsage = false;
+
+        static void Main(string[] args)
+        {
+            int chapterDepth = 10;
+
+            string query = null;
+            var s0 = args
+                .TakeWhile(a=>Regex.IsMatch(a,@"^[-/]"));
+            
+            var switches = s0
+                .Select(a=>Regex.Replace(a,"^[-/]","").ToLower())
+                .Aggregate(new StringBuilder(), (sb,w)=>sb.Append(w));
+
+            ProcessArgs(switches);
+
+            args = args.Skip(s0.Count()).ToArray();
+            
+            try
+            {
+                //Return top 20 most recently modified files
+                if (args.Length == 0)
+                {
+                    query = "SELECT TOP 20 System.ItemPathDisplay ";
+                    query += GetOptionalProperties();
+                    query += " FROM SystemIndex WHERE SCOPE='file:' ORDER BY System.DateModified DESC"; 
+                }
+                else
+                {
+                    //We have 1 or more args. We'll treat them as word to be joined with OR
+                    var keywords=new string[] {"OR", "AND"};
+
+                    query = "SELECT System.ItemPathDisplay"
+                        + GetOptionalProperties()
+                        + " FROM SystemIndex WHERE SCOPE='file:' AND (";
+
+                    int state = 1;  
+
+                    for (int i = 0; i<args.Length;++i)
+                    {
+                        var arg = args[i].Replace("'", @"\'");
+                        
+                        if (i>0 && keywords.Contains(arg))
+                        {
+                            query += string.Format(" {0} ", arg);
+                            state = 1;
+                        }
+                        else
+                        {
+                            query += state == 0 ? "OR" : "";
+                            query += string.Format(" CONTAINS(System.FileName, '{0}') ",arg);
+                            state = 0;
+                        }
+                    }
+
+                    query += ")";
+                }
+
+                var st = DateTime.Now;
+                ExecuteQuery(query, chapterDepth);
+                var tt = DateTime.Now - st;
+                Console.WriteLine("Took: {0} ms", tt.TotalMilliseconds);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                Console.WriteLine();
+                Usage();
+            }
+        }
+
+        private static string GetOptionalProperties()
+        {
+            string query = (OptModified ? ",System.DateModified" : "");
+            query += (OptSize ?  ",System.Size" : "");
+            return query;
+        }
+
+        private static void ProcessArgs(StringBuilder switches)
+        {
+            int state = 0;
+
+            for (int i = 0; i < switches.Length; ++i)
+            {
+                switch (switches[i])
+                {
+                    case '-':
+                    case '+':
+                        break;
+
+                    case 'l':
+                        OptModified = (state == 0);
+                        OptSize = (state == 0);
+                        break;
+
+                    case 'm':
+                        OptModified = (state == 0);
+                        break;
+
+                    case 's':
+                        OptSize = (state == 0);
+                        break;
+
+                    default:
+                        OptUsage = true;
+                        break;
+                }
+
+                if (OptUsage)
+                    Usage(); //and be done with it!
+
+                state = switches[i] == '-' ? 1 : 0;
+            }
+        }
+
+        // Display the result set recursively expanding chapterDepth deep
+        static void DisplayReader(OleDbDataReader myDataReader, ref uint count, uint alignment, int chapterDepth)
+        {
+            try
+            {
+                // compute alignment
+                StringBuilder indent = new StringBuilder((int)alignment);
+                indent.Append(' ', (int)alignment);
+
+                while (myDataReader.Read())
+                {
+                    // add alignment
+                    StringBuilder row = new StringBuilder(indent.ToString());
+
+                    // for all columns
+                    for (int i = 0; i < myDataReader.FieldCount; i++)
+                    {
+                        // null columns
+                        if (myDataReader.IsDBNull(i))
+                        {
+                            row.Append("NULL\t");
+                        }
+                        else
+                        {
+                            //vector columns
+                            object[] myArray = myDataReader.GetValue(i) as object[];
+                            if (myArray != null)
+                            {
+                                DisplayValue(myArray, row);
+                            }
+                            else
+                            {
+                                //check for chapter columns from "group on" queries
+                                if (myDataReader.GetFieldType(i).ToString() != "System.Data.IDataReader")
+                                {
+                                    //regular columns are displayed here
+                                    row.Append(myDataReader.GetValue(i));
+                                }
+                                else
+                                {
+                                    //for a chapter column type just display the colum name
+                                    row.Append(myDataReader.GetName(i));
+                                }
+                            }
+                            row.Append('\t');
+                        }
+                    }
+                    if (chapterDepth >= 0)
+                    {
+                        Console.WriteLine(row.ToString());
+                        count++;
+                    }
+                    // for each chapter column
+                    for (int i = 0; i < myDataReader.FieldCount; i++)
+                    {
+                        if (myDataReader.GetFieldType(i).ToString() == "System.Data.IDataReader")
+                        {
+                            OleDbDataReader Reader = myDataReader.GetValue(i) as OleDbDataReader;
+                            DisplayReader(Reader, ref count, alignment + 8, chapterDepth - 1);
+                        }
+                    }
+                }
+            }
+            finally
+            {
+                myDataReader.Close();
+                myDataReader.Dispose();
+            }
+        }
+
+        // display the value recursively
+        static void DisplayValue(object value, StringBuilder sb)
+        {
+            if (value != null)
+            {
+                if (value.GetType().IsArray)
+                {
+                    sb.Append("[");
+                    bool first = true;
+
+                    // display every element
+                    foreach (object subval in value as Array)
+                    {
+                        if (first)
+                        {
+                            first = false;
+                        }
+                        else
+                        {
+                            sb.Append("; ");
+                        }
+                        DisplayValue(subval, sb);
+                    }
+
+                    sb.Append("]");
+                }
+                else
+                {
+                    if (value.GetType() == typeof(double))
+                    {
+                        // Normal numeric formats round, but we want to report the actual round trip format
+                        sb.AppendFormat("{0:r}", value);
+                    }
+                    else
+                    {
+                        sb.Append(value);
+                    }
+                }
+            }
+        }
+
+        // Run a query and display the rowset up to chapterDepth deep
+        static void ExecuteQuery(string query, int chapterDepth)
+        {
+            OleDbDataReader myDataReader = null;
+            OleDbConnection myOleDbConnection = new OleDbConnection(connectionString);
+            OleDbCommand myOleDbCommand = new OleDbCommand(query, myOleDbConnection);
+            try
+            {
+                Console.WriteLine("Query=" + query);
+                myOleDbConnection.Open();
+                myDataReader = myOleDbCommand.ExecuteReader();
+                if (!myDataReader.HasRows)
+                {
+                    System.Console.WriteLine("Query returned 0 rows!");
+                    return;
+                }
+                uint count = 0;
+                DisplayReader(myDataReader, ref count, 0, chapterDepth);
+                Console.WriteLine("Rows+Chapters=" + count);
+            }
+            catch (System.Data.OleDb.OleDbException oleDbException)
+            {
+                Console.WriteLine("Got OleDbException, error code is 0x{0:X}L", oleDbException.ErrorCode);
+                Console.WriteLine("Exception details:");
+                for (int i = 0; i < oleDbException.Errors.Count; i++)
+                {
+                    Console.WriteLine("\tError " + i.ToString(CultureInfo.CurrentCulture.NumberFormat) + "\n" +
+                                      "\t\tMessage: " + oleDbException.Errors[i].Message + "\n" +
+                                      "\t\tNative: " + oleDbException.Errors[i].NativeError.ToString(CultureInfo.CurrentCulture.NumberFormat) + "\n" +
+                                      "\t\tSource: " + oleDbException.Errors[i].Source + "\n" +
+                                      "\t\tSQL: " + oleDbException.Errors[i].SQLState + "\n");
+                }
+                Console.WriteLine(oleDbException.ToString());
+            }
+            finally
+            {
+                // Always call Close when done reading.
+                if (myDataReader != null)
+                {
+                    myDataReader.Close();
+                    myDataReader.Dispose();
+                }
+                // Close the connection when done with it.
+                if (myOleDbConnection.State == System.Data.ConnectionState.Open)
+                {
+                    myOleDbConnection.Close();
+                }
+            }
+        }
+
+        static void Usage()
+        {
+            var a = Environment.GetCommandLineArgs()[0];
+            Console.WriteLine("Usage: \n");
+            Console.WriteLine(a + " [keyword] [AND|OR] [keyword]");
+            Console.WriteLine("");
+            Console.WriteLine("\nThis will find all files/folders in Windows Search Index where the Filename contains keyword");
+            Console.WriteLine("\n");
+            Console.WriteLine("    Options: ");
+            Console.WriteLine("      -l\t\tLong Displays DateModified and Size of files");
+            Console.WriteLine("      -m\t\tShow Date modified");
+            Console.WriteLine("      -s\t\tShow Size (NULL for folders)");
+            Console.WriteLine("      --\t\tNegate next switch (so --s means do not show size");
+            Console.WriteLine("\n");
+            Console.WriteLine("Options can be combined, so '-ms' is equivalent to '-m -s' and results in 'Show Date Modified and Size' in results");
+                        
+            Environment.Exit(0);
+        }
+    }
+}
---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("locate")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("locate")]
+[assembly: AssemblyCopyright("Copyright ©  2011")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("481c4bd0-01c9-4334-8481-848c6df01700")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
